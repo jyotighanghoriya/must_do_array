e1d5c8b9b4417cf9175dc0eefda1a577fe815304
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2021-06-05 14:06:43 +0530

    Update wave_array.py

--- wave_array.py
+++ wave_array.py
@@ -1,4 +1,4 @@
-#Method 1: O(n^2)
+#Method 1: O(n^2) : sort and swap adjacent elements
 class Solution:
     #Complete this function
     #Function to sort the array into a wave-like array.
@@ -9,7 +9,7 @@ class Solution:
             temp=A[i]
             A[i]=A[i+1]
             A[i+1]=temp
-#Method 2: O(n)
+#Method 2: O(n) :Swap elements at even location with even location if value at odd location is greater.
 class Solution:
     #Complete this function
     #Function to sort the array into a wave-like array.

3d61ad6738977eb9eb5093ea28f1bd75765fe18a
jyotighanghoriya <98652893+jyotighanghoriya@users.noreply.github.com>
2022-10-28 18:55:06 +0530

    Update 1. subarray_with_given_sum.cpp

--- 1. subarray_with_given_sum.cpp
+++ 1. subarray_with_given_sum.cpp
@@ -1,1 +1,23 @@
+#User function Template for python3
 
+#Function to find a continuous sub-array which adds up to a given number.
+class Solution:
+    def subArraySum(self,arr, n, s):
+        sum=arr[0]
+        start=0
+        for i in range(1,n+1):
+            # print(i)
+        
+            while(sum>s and start<i-1):
+                sum=sum-arr[start]
+                start=start+1
+                print("start=",start,sum)
+            if sum==s:
+                print("equal condition encountered",start+1,i)
+                return start+1,i
+            elif sum<s and i!=n:
+                print(i)
+                sum=sum+arr[i]
+                print("sum=",sum)
+        if start<=n:
+            return [-1]
